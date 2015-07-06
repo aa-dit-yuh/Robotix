d4ba962c327adfd7d137ba5de0544b6fa5070a2e
Aditya Narayan <narayanaditya95@gmail.com>
2015-07-06 16:52:52 +0530

    Maintain uniform code convention in base.js

--- Robotix/static/js/base.js
+++ Robotix/static/js/base.js
@@ -45,11 +45,10 @@ var Cl = window.Cl || {};
 $(document).ready(function() {
     // one page navigation
     $('#main_navigation_menu').onePageNav({
-            currentClass: 'active'
+        currentClass: 'active'
     });
     //WOW Scroll Spy
     var wow = new WOW({
-        //disabled for mobile
         mobile: true
     });
     wow.init();
@@ -67,86 +66,67 @@ $(document).ready(function(){
     });
 });
 
+
 //ScrollTop
 /**
     by Nemes Ioan Sorin - not an jQuery big fan 
     therefore this script is for those who love the old clean coding style  
     @id = the id of the element who need to bring  into view
-      
     Note : this demo scrolls about 12.700 pixels from Link1 to Link3
 */
-(function()
-{
+(function(){
       window.setTimeout = window.setTimeout; //
 })();
 
-      var smoothScr = {
-      iterr : 300, // set timeout miliseconds ..decreased with 1ms for each iteration
-        tm : null, //timeout local variable
-      stopShow: function()
-      {
+var smoothScr = {
+    iterr : 300, // set timeout miliseconds ..decreased with 1ms for each iteration
+    tm : null, //timeout local variable
+    stopShow: function(){
         clearTimeout(this.tm); // stopp the timeout
         this.iterr = 30; // reset milisec iterator to original value
-      },
-      getRealTop : function (el) // helper function instead of jQuery
-      {
+    },
+    getRealTop : function (el){
         var elm = el; 
         var realTop = 0;
-        do
-        {
-          realTop += elm.offsetTop;
-          elm = elm.offsetParent;
+        do{
+            realTop += elm.offsetTop;
+            elm = elm.offsetParent;
         }
         while(elm);
         return realTop;
-      },
-      getPageScroll : function()  // helper function instead of jQuery
-      {
+    },
+    getPageScroll : function(){
         var pgYoff = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
         return pgYoff;
-      },
-      anim : function (id) // the main func
-      {
+    },
+    anim : function (id){
         this.stopShow(); // for click on another button or link
         var eOff, pOff, tOff, scrVal, pos, dir, step;
-
         eOff = document.getElementById(id).offsetTop; // element offsetTop
-
         tOff =  this.getRealTop(document.getElementById(id).parentNode); // terminus point 
-
         pOff = this.getPageScroll(); // page offsetTop
-
         if (pOff === null || isNaN(pOff) || pOff === 'undefined') pOff = 0;
-
         scrVal = eOff - pOff; // actual scroll value;
-
-        if (scrVal > tOff) 
-        {
+        if (scrVal > tOff) {
           pos = (eOff - tOff - pOff); 
           dir = 1;
         }
-        if (scrVal < tOff)
-        {
+        if (scrVal < tOff){
           pos = (pOff + tOff) - eOff;
           dir = -1; 
         }
-        if(scrVal !== tOff) 
-        {
-          step = ~~((pos / 4) +1) * dir;
-
-          if(this.iterr > 1) this.iterr -= 1; 
-          else this.itter = 0; // decrease the timeout timer value but not below 0
-          window.scrollBy(0, step);
-          this.tm = window.setTimeout(function()
-          {
-             smoothScr.anim(id);  
-          }, this.iterr); 
+        if(scrVal !== tOff) {
+            step = ~~((pos / 4) +1) * dir;
+            if(this.iterr > 1) this.iterr -= 1; 
+            else this.itter = 0; // decrease the timeout timer value but not below 0
+            window.scrollBy(0, step);
+            this.tm = window.setTimeout(function(){
+                smoothScr.anim(id);
+            }, this.iterr);
         }  
-        if(scrVal === tOff) 
-        { 
-          this.stopShow(); // reset function values
-          return;
+        if(scrVal === tOff) { 
+            this.stopShow(); // reset function values
+            return;
         }
     }
- };
- 
+};
